066be4ac97c06def2324736c6d275f1292bd3fed
Nick Powell and Tyler Stephenson <tyler@>
2017-06-01 11:48:35 -0700

    add delete post.

--- app/routes/post.js
+++ app/routes/post.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('post', params.post_id);
   },
+  actions: {
+    delete(model) {
+      if (confirm('Are you sure you want to delete this post?')) {
+        model.destroyRecord();
+        this.transitionTo('index');
+      }
+    }
+  }
 });
